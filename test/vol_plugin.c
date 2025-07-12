efc4d82e9312ed5f129c2b1ea8663c40cac6704b
Ed <edwardjameshartnett@gmail.com>
2025-07-12 07:47:47 -0600

    test: add VOL connector file open/close registration test

--- test/vol_plugin.c
+++ test/vol_plugin.c
@@ -161,7 +161,60 @@ error:
 
 } /* end test_registration_by_name() */
 
-
+/*-------------------------------------------------------------------------
+ * Function:    test_registration_by_name()
+ *
+ * Purpose:     Tests if we can load, register, and close a VOL
+ *              connector by name.
+ *
+ * Return:      SUCCEED/FAIL
+ *
+ *-------------------------------------------------------------------------
+ */
+static herr_t
+test_file_open_close(void)
+{
+    htri_t  is_registered   = FAIL;
+    hid_t   vol_id          = H5I_INVALID_HID;
+
+    TESTING("VOL file open/close");
+
+    /* The VOL connector should not be registered at the start of the test */
+    if((is_registered = H5VLis_connector_registered_by_name(GRIB2_VOL_CONNECTOR_NAME)) < 0)
+        TEST_ERROR;
+    if(true == is_registered)
+        FAIL_PUTS_ERROR("VOL connector is inappropriately registered");
+
+    /* Register the connector by name */
+    if((vol_id = H5VLregister_connector_by_name(GRIB2_VOL_CONNECTOR_NAME, H5P_DEFAULT)) < 0)
+        TEST_ERROR;
+
+    /* The connector should be registered now */
+    if((is_registered = H5VLis_connector_registered_by_name(GRIB2_VOL_CONNECTOR_NAME)) < 0)
+        TEST_ERROR;
+    if(false == is_registered)
+        FAIL_PUTS_ERROR("VOL connector was not registered");
+
+    /* Unregister the connector */
+    if(H5VLunregister_connector(vol_id) < 0)
+        TEST_ERROR;
+
+    /* The connector should not be registered now */
+    if((is_registered = H5VLis_connector_registered_by_name(GRIB2_VOL_CONNECTOR_NAME)) < 0)
+        TEST_ERROR;
+    if(true == is_registered)
+        FAIL_PUTS_ERROR("VOL connector is inappropriately registered");
+
+    PASSED();
+    return SUCCEED;
+
+error:
+    H5E_BEGIN_TRY {
+        H5VLunregister_connector(vol_id);
+    } H5E_END_TRY;
+    return FAIL;
+
+} /* end test_file_open_close() */
 /*-------------------------------------------------------------------------
  * Function:    test_multiple_registration()
  *
@@ -305,6 +358,7 @@ main(void)
         printf("NULL\n");
 
     nerrors += test_registration_by_name() < 0          ? 1 : 0;
+    nerrors += test_file_open_close() < 0          ? 1 : 0;
     nerrors += test_registration_by_value() < 0         ? 1 : 0;
     nerrors += test_multiple_registration() < 0         ? 1 : 0;
     nerrors += test_getters() < 0                       ? 1 : 0;
