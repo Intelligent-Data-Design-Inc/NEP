4b1920e4fcbc5fc3cf8203d9fc23cd9260d45e52
Ed <edwardjameshartnett@gmail.com>
2025-11-23 09:11:19 -0700

    docs: update CDF support documentation for Sprint 3 UDF handler integration
    Update README and roadmap to reflect Sprint 3 CDF UDF handler library build integration. Change CDF option description from "library detection only" to active UDF handler build with stubbed implementation. Add detailed Sprint 3 tasks covering CMake/Autotools integration, code preparation, attribution updates, and build verification. Note that Sprint 3 provides build infrastructure only - functional implementation deferred to Sprint 4.
    
    -

---
+++ src/cdffile.c
@@ -0,0 +1,259 @@
+/**
+ * @file
+ * @internal The CDF file functions. These provide a read-only
+ * interface to CDF SD files.
+ *
+ * @author Edward Hartnett
+ * @date 2025-11-23
+ * @copyright Intelligent Data Design, Inc. All rights reserved.
+ */
+
+#include "config.h"
+#include "nc4internal.h"
+#include "hdf4dispatch.h"
+#include <mfhdf.h>
+
+#define NUM_TYPES 12 /**< Number of netCDF atomic types. */
+
+extern int nc4_vararray_add(NC_GRP_INFO_T *grp, NC_VAR_INFO_T *var);
+
+/** @internal These flags may not be set for open mode. */
+static const int
+ILLEGAL_OPEN_FLAGS = (NC_MMAP|NC_64BIT_OFFSET|NC_DISKLESS|NC_WRITE);
+
+/** @internal NetCDF atomic type names. */
+static const char*
+nc_type_name_g[NUM_TYPES] = {"char", "byte", "short", "int", "float", "double",
+                             "ubyte", "ushort", "uint", "int64", "uint64",
+                             "string"};
+
+/** @internal NetCDF atomic type sizes. */
+static const size_t
+nc_type_size_g[NUM_TYPES] = {sizeof(char), sizeof(char), sizeof(short),
+                             sizeof(int), sizeof(float), sizeof(double),
+                             sizeof(unsigned char), sizeof(unsigned short),
+                             sizeof(unsigned int), sizeof(long long),
+                             sizeof(unsigned long long), sizeof(char *)};
+
+/**
+ * @internal Recursively delete the data for a group (and everything
+ * it contains) in our internal metadata store.
+ *
+ * @param grp Pointer to group info struct.
+ *
+ * @return ::NC_NOERR No error.
+ * @author Edward Hartnett
+ */
+static int
+hdf4_rec_grp_del(NC_GRP_INFO_T *grp)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
+
+/**
+ * @internal Given an CDF type, set a pointer to netcdf type.
+ *
+ * See http://www.hdfgroup.org/training/HDFtraining/UsersGuide/Fundmtls.fm3.html
+ * for more information re: CDF types.
+ *
+ * @param h5 Pointer to HDF5 file info struct.
+ * @param hdf4_typeid Type ID for hdf4 datatype.
+ * @param xtype Pointer that gets netcdf type. Ignored if NULL.
+ * @param endniannessp Pointer that gets endianness. Ignored if NULL.
+ * @param type_sizep Pointer that gets type size. Ignored if NULL.
+ * @param type_name Pointer that gets the type name. Ignored if NULL.
+ *
+ * @return ::NC_NOERR No error.
+ * @author Ed Hartnett
+ */
+static int
+hdf4_type_info(NC_FILE_INFO_T *h5, int32 hdf4_typeid, nc_type* xtypep,
+               int *endiannessp, size_t *type_sizep, char *type_name)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
+
+/**
+ * @internal Set the type of a netCDF-4 variable.
+ *
+ * @param xtype A netcdf type.
+ * @param endianness The endianness of the data.
+ * @param type_size The size in bytes of one element of this type.
+ * @param type_name A name for the type.
+ * @param typep Pointer to a pointer that gets the TYPE_INFO_T struct.
+ *
+ * @return ::NC_NOERR No error.
+ * @author Ed Hartnett
+ */
+static int
+nc4_set_var_type(nc_type xtype, int endianness, size_t type_size, char *type_name,
+                 NC_TYPE_INFO_T **typep)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
+
+/**
+ * @internal Read an attribute from a CDF file.
+ *
+ * @param h5 Pointer to the file metadata struct.
+ * @param var Pointer to variable metadata struct or NULL for global
+ * attributes.
+ * @param a Index of attribute to read.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EHDFERR CDF error.
+ * @return ::NC_EATTMETA Error reading CDF attribute.
+ * @return ::NC_ENOMEM Out of memory.
+ * @author Ed Hartnett
+ */
+static int
+hdf4_read_att(NC_FILE_INFO_T *h5, NC_VAR_INFO_T *var, int a)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
+
+/**
+ * @internal Read a CDF dimension. As new dimensions are found, add
+ * them to the metadata list of dimensions.
+ *
+ * @param h5 Pointer to the file metadata struct.
+ * @param var Pointer to variable metadata struct or NULL for global
+ * attributes.
+ * @param rec_dim_len Actual length of first dim for this SD.
+ * @param d Dimension index for this SD.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EHDFERR CDF error.
+ * @return ::NC_EDIMMETA Error reading CDF dimension info.
+ * @return ::NC_ENOMEM Out of memory.
+ * @return ::NC_EMAXNAME Name too long.
+ * @author Ed Hartnett
+ */
+static int
+hdf4_read_dim(NC_FILE_INFO_T *h5, NC_VAR_INFO_T *var, int rec_dim_len, int d)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
+
+/**
+ * @internal Create a new variable and insert int relevant lists
+ *
+ * @param grp the containing group
+ * @param name the name for the new variable
+ * @param ndims the rank of the new variable
+ * @param format_var_info Pointer to format-specific var info struct.
+ * @param var Pointer in which to return a pointer to the new var.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_ENOMEM Out of memory.
+ * @author Ed Hartnett
+ */
+static int
+nc4_var_list_add_full(NC_GRP_INFO_T* grp, const char* name, int ndims, nc_type xtype,
+                      int endianness, size_t type_size, char *type_name, void *fill_value,
+                      int contiguous, size_t *chunksizes, void *format_var_info,
+                      NC_VAR_INFO_T **var)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
+
+/**
+ * @internal Read a CDF variable, including its associated dimensions
+ * and attributes.
+ *
+ * @param h5 Pointer to the file metadata struct.
+ * @param v Index of variable to read.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EHDFERR CDF error.
+ * @return ::NC_EDIMMETA Error reading CDF dimension info.
+ * @return ::NC_EVARMETA Error reading CDF dataset or att.
+ * @return ::NC_EATTMETA Error reading CDF attribute.
+ * @return ::NC_ENOMEM Out of memory.
+ * @return ::NC_EMAXNAME Name too long.
+ * @author Ed Hartnett
+ */
+static int
+hdf4_read_var(NC_FILE_INFO_T *h5, int v)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
+
+/**
+ * @internal Open a CDF SD file for read-only access.
+ *
+ * @param path The file name of the file.
+ * @param mode The open mode flag.
+ * @param basepe Ignored by this function.
+ * @param chunksizehintp Ignored by this function.
+ * @param parameters pointer to struct holding extra data (e.g. for
+ * parallel I/O) layer. Ignored if NULL. Ignored by this function.
+ * @param dispatch Pointer to the dispatch table for this file.
+ * @param nc_file Pointer to an instance of NC. The ncid has already
+ * been assigned, and is in nc_file->ext_ncid.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EINVAL Invalid input.
+ * @return ::NC_EHDFERR Error from CDF layer.
+ * @return ::NC_ENOMEM Out of memory.
+ * @author Ed Hartnett
+ */
+int
+NC_CDF_open(const char *path, int mode, int basepe, size_t *chunksizehintp,
+             void *parameters, const NC_Dispatch *dispatch, int ncid)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_ENOTNC4; /* Placeholder return */
+}
+
+/**
+ * @internal Abort (close) the CDF file.
+ *
+ * @param ncid File ID.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EBADID Bad ncid.
+ * @return ::NC_EHDFERR Error from CDF layer.
+ * @author Ed Hartnett
+ */
+int
+NC_CDF_abort(int ncid)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_ENOTNC4; /* Placeholder return */
+}
+
+/**
+ * @internal Close the CDF file.
+ *
+ * @param ncid File ID.
+ * @param ignore Ignore this pointer.
+ *
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EBADID Bad ncid.
+ * @return ::NC_EHDFERR Error from CDF layer.
+ * @author Ed Hartnett
+ */
+int
+NC_CDF_close(int ncid, void *ignore)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    return NC_NOERR; /* Placeholder return */
+}
---
+++ src/cdffunc.c
@@ -0,0 +1,71 @@
+/**
+ * @file @internal CDF functions.
+ *
+ * @author Edward Hartnett
+ * @date 2025-11-23
+ * @copyright Intelligent Data Design, Inc. All rights reserved.
+ */
+
+#include "config.h"
+#include "nc4internal.h"
+#include "hdf4dispatch.h"
+#include <mfhdf.h>
+
+/**
+ * @internal Get the format (i.e. NC_FORMAT_NC_CDF) of an open CDF
+ * file.
+ *
+ * @param ncid File ID (ignored).
+ * @param formatp Pointer that gets the constant indicating format.
+
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EBADID Bad ncid.
+ * @author Edward Hartnett
+ */
+int
+NC_CDF_inq_format(int ncid, int *formatp)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    /* CDF is the format. */
+    /* if (formatp) */
+    /*     *formatp = NC_FORMATX_NC_CDF; */
+
+    return NC_ENOTNC4; /* Placeholder return */
+}
+
+/**
+ * @internal Return the extended format (i.e. the dispatch model),
+ * plus the mode associated with an open file.
+ *
+ * @param ncid File ID.
+ * @param formatp a pointer that gets the extended format. CDF files
+ * will always get NC_FORMATX_NC_CDF.
+ * @param modep a pointer that gets the open/create mode associated with
+ * this file. Ignored if NULL.
+
+ * @return ::NC_NOERR No error.
+ * @return ::NC_EBADID Bad ncid.
+ * @author Edward Hartnett
+ */
+int
+NC_CDF_inq_format_extended(int ncid, int *formatp, int *modep)
+{
+    /* Function body commented out for v1.3.0 Sprint 3 - UDF skeleton only */
+    /* Implementation will be added in Sprint 4 */
+    /* NC *nc; */
+    /* int retval; */
+
+    /* LOG((2, "%s: ncid 0x%x", __func__, ncid)); */
+
+    /* if ((retval = nc4_find_nc_grp_h5(ncid, &nc, NULL, NULL))) */
+    /*     return NC_EBADID; */
+
+    /* if (modep) */
+    /*     *modep = nc->mode|NC_NETCDF4; */
+
+    /* if (formatp) */
+    /*     *formatp = NC_FORMATX_NC_CDF; */
+
+    return NC_ENOTNC4; /* Placeholder return */
+}
