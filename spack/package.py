36bcb23a35dfd57819eb7053d1f1801ceb38093e
Ed <edwardjameshartnett@gmail.com>
2025-12-09 11:07:48 -0700

    ci: optimize Spack workflow for disk space and disable MPI dependencies
    Remove no-docs variant test (docs now disabled by default). Add disk space cleanup step to remove unnecessary packages (dotnet, ghc, boost). Configure Spack to use /tmp/spack-stage for builds and clean up aggressively. Set packages to prefer external/system installations with buildable:false by default, only allowing nep, netcdf-c, hdf5, lz4, and bzip2 to build. Disable MPI in hdf5 and netcdf-c dependencies since NEP doesn't require

--- spack/package.py
+++ spack/package.py
@@ -22,12 +22,12 @@ class Nep(CMakePackage):
     version("1.3.0", sha256="acf8f8a1360b521cef3f6f57fdeced64d2d78e765e8fd644ce435487fe74a003")
     version("1.0.0", sha256="c996da322c29e184a363ce013e5d44e773525e326a1d8022d69b5df46c032f54")
 
-    variant("docs", default=True, description="Build documentation with Doxygen")
+    variant("docs", default=False, description="Build documentation with Doxygen")
     variant("lz4", default=True, description="Enable LZ4 compression support")
     variant("bzip2", default=True, description="Enable BZIP2 compression support")
 
-    depends_on("netcdf-c@4.9:", type=("build", "link"))
-    depends_on("hdf5@1.12:+hl", type=("build", "link"))
+    depends_on("netcdf-c@4.9:~mpi", type=("build", "link"))
+    depends_on("hdf5@1.12:+hl~mpi", type=("build", "link"))
     depends_on("lz4", when="+lz4", type=("build", "link"))
     depends_on("bzip2", when="+bzip2", type=("build", "link"))
     depends_on("doxygen", when="+docs", type="build")
@@ -35,7 +35,10 @@ class Nep(CMakePackage):
     def cmake_args(self):
         args = [
             self.define_from_variant("BUILD_DOCUMENTATION", "docs"),
+            self.define_from_variant("BUILD_LZ4", "lz4"),
+            self.define_from_variant("BUILD_BZIP2", "bzip2"),
             self.define("BUILD_TESTING", self.run_tests),
+            self.define("ENABLE_FORTRAN", False),
         ]
         return args
 
