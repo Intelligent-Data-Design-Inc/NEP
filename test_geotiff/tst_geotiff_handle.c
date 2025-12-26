7d8e1d221d63929903d1ac61aec538de8cf9c3a7
Ed <edwardjameshartnett@gmail.com>
2025-12-26 12:42:39 -0700

    Use correct NC_UDF1 constant from netcdf.h
    Fixed nc_def_user_format() to use NC_UDF1 (0x0080) instead of
    NC_FORMATX_UDF1. NC_UDF1 is the proper mode flag defined in
    netcdf.h for user-defined format slot 1.
    
    This should fix the NC_EINVAL error when registering the GeoTIFF
    UDF handler.

--- test_geotiff/tst_geotiff_handle.c
+++ test_geotiff/tst_geotiff_handle.c
@@ -314,8 +314,8 @@ main(void)
     }
     
     /* Register GeoTIFF UDF handler with NetCDF-C */
-    /* GeoTIFF uses UDF1 slot - pass the format constant as the mode flag */
-    int reg_ret = nc_def_user_format(NC_FORMATX_UDF1, (NC_Dispatch*)GEOTIFF_dispatch_table, NULL);
+    /* GeoTIFF uses UDF1 slot - NC_UDF1 is defined in netcdf.h as 0x0080 */
+    int reg_ret = nc_def_user_format(NC_UDF1, (NC_Dispatch*)GEOTIFF_dispatch_table, NULL);
     if (reg_ret != NC_NOERR)
     {
         printf("ERROR: Failed to register GeoTIFF UDF handler: %s (code %d)\n", nc_strerror(reg_ret), reg_ret);
