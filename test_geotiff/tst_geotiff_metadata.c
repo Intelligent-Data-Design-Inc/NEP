ee200bb0ad5689bdd20f489c32a0fe4033a8d4d2
Ed <edwardjameshartnett@gmail.com>
2025-12-30 07:41:35 -0700

    merged

--- test_geotiff/tst_geotiff_metadata.c
+++ test_geotiff/tst_geotiff_metadata.c
@@ -13,9 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "geotiffdispatch.h"
-#include "geotiffdispatch.h"
 
-#define NASA_DATA_DIR "./"
 #define NASA_DATA_DIR "./"
 #define ERR_CHECK(ret) do { if ((ret) != NC_NOERR) { \
     printf("Error at line %d: %s\n", __LINE__, nc_strerror(ret)); \
@@ -299,7 +297,6 @@ main(void)
 {
     int err = 0;
     char magic_number[4] = "II*";
-    char magic_number[4] = "II*";
 
     printf("\n*** Testing GeoTIFF Phase 2: Dispatch Integration and Metadata Extraction ***\n");
 
@@ -323,18 +320,18 @@ main(void)
     /* Test dispatch layer integration */
     err += test_dispatch_integration();
     
-    // /* Test metadata extraction */
-    // err += test_dimension_extraction();
-    // err += test_variable_extraction();
+    /* Test metadata extraction */
+    err += test_dimension_extraction();
+    err += test_variable_extraction();
     
-    // /* Test format inquiry */
-    // err += test_format_inquiry();
+    /* Test format inquiry */
+    err += test_format_inquiry();
     
-    // /* Test error handling */
-    // err += test_gtifnew_error_handling();
+    /* Test error handling */
+    err += test_gtifnew_error_handling();
     
-    // /* Test with multiple files */
-    // err += test_second_nasa_file();
+    /* Test with multiple files */
+    err += test_second_nasa_file();
 
     if (err)
     {
