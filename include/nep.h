17392b71ad8bf65a7783cbd8660715143a72b55d
Ed <edwardjameshartnett@gmail.com>
2026-03-06 11:44:03 -0700

    ci: remove compression matrix dependency for CDF builds, expand GeoTIFF test coverage, remove GRIB2/JPEG references
    - Remove compression matrix condition from CDF enable logic (both CMake and Autotools)
    - Add tst_geotiff_self_load and tst_geotiff_ncrc_autoload to Valgrind and ASAN test suites
    - Delete all GRIB2 and JPEG build infrastructure (configure flags, headers, conditionals)
    - Remove grib2dispatch.h and grib2logging.h header files
    - Remove GRIB2_LOGGING and BUILD_JPEG defines from configure

--- include/nep.h
+++ include/nep.h
@@ -11,8 +11,7 @@
  * - UDF0: GeoTIFF BigTIFF (magic: "II+")
  * - UDF1: GeoTIFF standard TIFF (magic: "II*")
  * - UDF2: NASA CDF format (magic: 0xCDF30001)
- * - UDF3: GRIB2 format (reserved)
- * - UDF4-UDF9: Reserved for future use
+ * - UDF3-UDF9: Reserved for future use
  *
  * @author Edward Hartnett
  * @date Nov 13, 2025
@@ -31,14 +30,8 @@
 /** The filter ID for LZ4 compression. */
 #define LZ4_ID 32004
 
-/** The filter ID for JPEG compression. */
-#define JPEG_ID 32019
-
 /** The filter ID for LZF compression. */
 #define LZF_ID 32000
-
-/** Number of elements in JPEG parameter array. */
-#define NCC_JPEG_NELEM 4
 
 #if defined(__cplusplus)
 extern "C" {
@@ -49,10 +42,6 @@ extern "C" {
     int nc_inq_var_bzip2(int ncid, int varid, int *bzip2p, int *levelp);
     int nc_def_var_lz4(int ncid, int varid, int level);
     int nc_inq_var_lz4(int ncid, int varid, int *lz4p, int *levelp);
-    int nc_def_var_jpeg(int ncid, int varid, int quality_factor, int nx,
-			int ny, int rgb);
-    int nc_inq_var_jpeg(int ncid, int varid, int *jpegp, int *quality_factorp, int *nxp,
-			int *nyp, int *rgbp);
     int nc_def_var_lzf(int ncid, int varid);
     int nc_inq_var_lzf(int ncid, int varid, int *lzfp);
     
@@ -73,9 +62,6 @@ extern "C" {
 
 /** NASA CDF format uses UDF2 slot */
 #define NEP_UDF_CDF NC_UDF2
-
-/** GRIB2 format uses UDF3 slot (reserved) */
-#define NEP_UDF_GRIB2 NC_UDF3
 
 /** @} */
 
@@ -105,9 +91,6 @@ extern "C" {
 
 /** NASA CDF format display name */
 #define NEP_FORMAT_NAME_CDF "NASA CDF"
-
-/** GRIB2 format display name */
-#define NEP_FORMAT_NAME_GRIB2 "GRIB2"
 
 /** @} */
 
