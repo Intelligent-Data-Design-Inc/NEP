2881df8d8be1a351b5caf54602e32266cdc2c5c4
Ed <edwardjameshartnett@gmail.com>
2026-05-31 16:07:42 -0600

    fix

--- examples/f_netcdf-4/f_compression.f90
+++ examples/f_netcdf-4/f_compression.f90
@@ -143,7 +143,7 @@ program f_compression
    do i = 1, NUM_TESTS
       print '(A35, F12.3, F12.3, F12.2, F9.2, A1)', &
          trim(test_names(i)), write_times(i), read_times(i), &
-         file_sizes(i) / 1048576.0, compression_ratios(i), "x"
+         real(file_sizes(i), 8) / 1048576.0_8, compression_ratios(i), "x"
    end do
    
    ! Print recommendations
@@ -235,7 +235,7 @@ contains
       
       inquire(file=filename, exist=file_exists, size=file_size)
       if (file_exists) then
-         print *, "File size: ", file_size, " bytes (", file_size / 1048576.0, " MB)"
+         print *, "File size: ", file_size, " bytes (", real(file_size, 8) / 1048576.0_8, " MB)"
       end if
       
       print *, "Write time: ", write_time, " seconds"
