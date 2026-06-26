dcc4f153b56f06bbea16dcb1a6c762b6b3e152b3
Ed <edwardjameshartnett@gmail.com>
2026-06-26 14:41:57 -0600

    Add Fortran NcZarr example with build system integration and test validation
    - Add f_nczarr_simple.f90 demonstrating NcZarr with Fortran column-major ordering
    - Update CMakeLists.txt: add f_nczarr_simple executable gated on ENABLE_FORTRAN and NetCDF_Fortran_FOUND
    - Update Makefile.am: add f_nczarr_simple inside ENABLE_FORTRAN conditional
    - Create test_f_nczarr_simple.sh.in validation script
    - Add test_f_nczarr_simple.sh to AC_CONFIG_FILES in configure.ac
    - Derive HDF5 lib dir from nc-config --c

--- examples/nczarr/test_nczarr_simple.sh
+++ examples/nczarr/test_nczarr_simple.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Run nczarr_simple and validate output CDL
+# Edward Hartnett, Intelligent Data Design, Inc.
+# 2026-06-26
 
 if [[ -n "${LD_LIBRARY_PATH:-}" ]]; then
-    export LD_LIBRARY_PATH=/home/ed/NEP/src/.libs:/usr/local/hdf5-2.1.0/lib:/usr/local/netcdf-c-4.10.0/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=/home/ed/NEP/src/.libs:/usr/local/hdf5-2.1.0/lib:/usr/local/netcdf-c-4.10.0/lib:/usr/local/netcdf-fortran/lib:$LD_LIBRARY_PATH
 else
-    export LD_LIBRARY_PATH=/home/ed/NEP/src/.libs:/usr/local/hdf5-2.1.0/lib:/usr/local/netcdf-c-4.10.0/lib
+    export LD_LIBRARY_PATH=/home/ed/NEP/src/.libs:/usr/local/hdf5-2.1.0/lib:/usr/local/netcdf-c-4.10.0/lib:/usr/local/netcdf-fortran/lib
 fi
 
 ./nczarr_simple
